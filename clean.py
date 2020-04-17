353b3acc0ab3062857c5dfe100711574569a0100
Xandy Almeida <xandy_yt@hotmail.com>
2020-04-17 00:44:24 -0300

    comando de limpar prefetch criado

--- clean.py
+++ clean.py
@@ -4,6 +4,7 @@ import time
 import asyncio
 
 temp = 'C:\\Windows\\Temp'
+temp2 = 'C:\\Users\\%USERPROFILE%\\AppData\\Local\\Temp'
 prefetch = 'C:\\Windows\\Prefetch'
 confirmar = 'Deseja excluir esses arquivos? S/n ==> '
 
@@ -19,7 +20,6 @@ def main():
 	print('2 - Limpar a pasta %TEMP%')
 	print('3 - Limpar a pasta Prefetch')
 	print('4 - Limpar todas pastas acima')
-	print('5 - Configurar pasta %TEMP%')
 	print('0 - Sair')
 	print('\n')
 	resposta = input('Escolha uma opcao: ')
@@ -32,12 +32,26 @@ def main():
 			print('Pasta TEMP vazia... Retornando para o menu...')
 			time.sleep(2)
 			main()
+
 	elif resposta == '2':
-		clearOutherTemp()
+		if os.listdir(temp2):
+			clearOutherTemp()
+		else:
+			print('Pasta %TEMP% vazia... Retornando para o menu...')
+			time.sleep(2)
+			main()
+
 	elif resposta == '3':
-		clearPrefetch()
+		if os.listdir(prefetch):
+			clearPrefetch()
+		else:
+			print('Pasta Prefetch vazia... Retornando para o menu...')
+			time.sleep(2)
+			main()
+
 	elif resposta == '4':
 		clearAll()
+
 	elif resposta == '0':
 		print('Saindo...')
 		time.sleep(1)
@@ -48,6 +62,7 @@ def main():
 		main()
 
 
+
 def clearTemp():
 	os.chdir(temp)
 	print('')
@@ -55,6 +70,7 @@ def clearTemp():
 		print(files)
 	print('')
 	resposta = str(input(confirmar))
+
 	if resposta == 'S' or resposta == 's':
 		print('Limpando...')
 		time.sleep(1)
@@ -67,6 +83,7 @@ def clearTemp():
 			main()
 		else:
 			print('Algo deu errado...')
+
 	elif resposta == 'N' or resposta == 'n':
 		print('Cancelando...')
 		time.sleep(1)
@@ -77,27 +94,54 @@ def clearTemp():
 		clearTemp()
 
 def clearOutherTemp():
-	print('clearOutherTemp')
-
-def clearPrefetch():
-	os.chdir(prefetch)
+	os.chdir(temp2)
 	print('')
-	for files in os.listdir(prefetch):
-		print(files)
+	for file in os.listdir(temp2):
+		print(file)
 	print('')
 	resposta = str(input(confirmar))
+
 	if resposta == 'S' or resposta == 's':
 		print('Limpando...')
 		time.sleep(1)
-		os.system('rmdir %s /S /Q'%prefetch)
+		os.system('rmdir %s /S /Q'%temp2)
 		time.sleep(1)
-		if not os.listdir(prefetch):
+		if not os.listdir(temp2):
 			print('')
 			print('Pasta TEMP limpada com sucesso... Retornando para o menu...')
 			time.sleep(1)
 			main()
 		else:
 			print('Algo deu errado...')
+
+
+def clearPrefetch():
+	# os.chdir acessa a pasta prefetch
+	os.chdir(prefetch)
+	print('')
+	for files in os.listdir(prefetch):
+		print(files)
+
+	print('')
+	resposta = str(input(confirmar))
+
+	if resposta == 'S' or resposta == 's':
+		print('Limpando...')
+		time.sleep(1)
+		try:
+			os.system('rmdir %s /S /Q'%prefetch)
+		except:
+			print('Erro ao tentar excluir os arquivos...')
+		time.sleep(1)
+
+		if not os.listdir(prefetch):
+			print('')
+			print('Pasta Prefetch limpada com sucesso... Retornando para o menu...')
+			time.sleep(1)
+			main()
+		else:
+			print('Algo deu errado...')
+
 	elif resposta == 'N' or resposta == 'n':
 		print('Cancelando...')
 		time.sleep(1)
